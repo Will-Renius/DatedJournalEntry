165e472734d3a6f6af629731522122b49f109db4
Will-Renius <reniuswi@msu.edu>
2018-10-30 07:04:49 -0400

    switch to simple date format
    date locale options are not supported by google apps, so switching to local date strings

--- code.js
+++ code.js
@@ -15,8 +15,12 @@ function onInstall() {
     
     //appends new journal template above end position
     function newEntry() {
+      Logger.log("newEntry()");
+    
       var doc = DocumentApp.getActiveDocument();
-      var new_pos = insertDate();
+      var dateSettings = getDateSettings();
+      var dateString = getDateString(dateSettings)
+      var new_pos = insertDate(dateString);
       doc.setCursor(new_pos);
       
     }
@@ -28,29 +32,26 @@ function onInstall() {
     }
     
     function setDateDefaults(){
-      var userProperties = PropertiesService.getUserProperties();
-      var options = { weekday: "long", year: 'numeric', month: 'numeric', day: 'numeric', hour:"2-digit", minute:"numeric", second:"numeric" };
-      Logger.log(options);
-      var stringOptions = JSON.stringify(options);
-      Logger.log(stringOptions);
-      userProperties.setProperty('DATE_OPTIONS', stringOptions);
-      var language = (Session.getActiveUserLocale()) ? Session.getActiveUserLocale() : "en"; 
-      userProperties.setProperty('DATE_LANGUAGE', language);
-      var today  = new Date(); 
-      
-      Logger.log(today.toLocaleDateString(language, options));
-      Logger.log(today.toLocaleString("de", options));
-      Logger.log(today.toLocaleString("en-US", { weekday: "long", year: 'numeric', month: 'numeric', day: 'numeric', hour:"2-digit", minute:"numeric", second:"numeric" }));
+      Logger.log("setDateDefaults()");
     
-    //  userProperties.setProperties(options);
+      var userProperties = PropertiesService.getUserProperties();
+      var dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+      userProperties.setProperty('DATE_FORMAT', dateFormat);
+      var timezone = Session.getScriptTimeZone();
+      userProperties.setProperty('DATE_TIMEZONE', timezone);
+      var formattedDate = Utilities.formatDate(new Date(), timezone, dateFormat);
+      Logger.log(formattedDate);
     }
     
     function quickTest(){
       setDateDefaults();
-      getDateString();
+      var dateSettings = getDateSettings();
+      getDateString(dateSettings);
       }
       
     function updateDateSettings(options){
+      Logger.log("updateDateSettings()");
+    
       Logger.log(options);
       var dateOptions =  JSON.stringify(options["dateOptions"]);
       Logger.log(dateOptions);
@@ -58,29 +59,38 @@ function onInstall() {
       userProperties.setProperty('DATE_LANGUAGE', options["language"]);
       userProperties.setProperty('DATE_OPTIONS',dateOptions);
     }
-    
-    function getDateString(){
+    function getDateSettings(){
+      Logger.log("getDateSettings()");
       var userProperties = PropertiesService.getUserProperties();
-      Logger.log(userProperties);
-      var rawDateOptions = userProperties.getProperty("DATE_OPTIONS");
-      Logger.log(rawDateOptions);
-      var dateOptions = JSON.parse(rawDateOptions);
-      Logger.log(dateOptions);
+      var dateFormatString = userProperties.getProperty("DATE_FORMAT");
+      Logger.log(dateFormatString);
     
-      var language = userProperties.getProperty("DATE_LANGUAGE");
-      Logger.log(language);
+      var dateTimeZone = userProperties.getProperty("DATE_TIMEZONE");
+      Logger.log(dateTimeZone);
+      
+      dateSettings = {timeZone:dateTimeZone,formatString:dateFormatString};
+      return(dateSettings);
+    }
+    function getDateString(dateSettings){
+      Logger.log("getDateString()");
+    
       var today  = new Date(); 
-      Logger.log(language);
-      Logger.log(today.toLocaleString(language, dateOptions));
-      return(today.toLocaleString(language, dateOptions));
+      Logger.log("Getting Date String");
+      Logger.log(dateSettings["timeZone"]);
+      Logger.log(dateSettings["formatString"]);
+    
+      var formattedDate = Utilities.formatDate(new Date(),dateSettings["timeZone"] , dateSettings["formatString"]);
+      Logger.log(formattedDate);
+      return(formattedDate)
     }
     
-    function insertDate() {
-       
+    function insertDate(dateString) {
+      Logger.log("insertDate()");
+    
       var body = DocumentApp.getActiveDocument().getBody();
-      var date = getDateString();
+      
       // Append a paragraph, with heading 1.
-      var par1 = body.appendParagraph(date);
+      var par1 = body.appendParagraph(dateString);
       par1.setHeading(DocumentApp.ParagraphHeading.HEADING1);
       
       var par2 = body.appendParagraph("\n");
